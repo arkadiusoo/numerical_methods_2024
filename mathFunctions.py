130e39ee9019539501db79196502c2e62c8e91f2
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-03-05 18:15:55 +0100

    improvments

--- mathFunctions.py
+++ mathFunctions.py
@@ -1,33 +1,44 @@
 import math
 
-def schematHornera (wspolczynniki, x):
+collection = {
+    "0" : ["3sin(2x)", ["sin", 3, 2, 0,0]], #[funTryg, wspolczynnikPrzyY, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY,]
+    "1" : ["(2^3x-6)+11", [2,3,-6,11]], #podstawa, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY
 
-    wynik = 0
-
-    for i in range(len(wspolczynniki)):
-        wynik = wynik * x + wspolczynniki[i]
-    return wynik
+}
 
 def potegowanie (x,potega):
     wynik = 1
     for i in range(potega):
         wynik *=x
     return wynik
+def rozwiazWielomioan (wspolczynniki, x):
+    wynik = 0
+    for i in range(len(wspolczynniki)):
+        wynik = wynik * x + wspolczynniki[i]
+    return wynik
 
 
-def rozwiazWykladnicze(podstawa, x, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY):
-    return potegowanie(podstawa,wspolczynnikPrzyX * x+ wspolczynnikDoX) + wspolczynnikDoY
+def rozwiazWykladnicze(wspolczynniki, x):
+    #wspolczynniki = [podstawa, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY]
+    # return potegowanie(wspolczynniki[0],wspolczynniki[1] * x+ wspolczynniki[2]) + wspolczynniki[3]
+    return pow(wspolczynniki[0],((wspolczynniki[1] * x +  wspolczynniki[2]) ))+ wspolczynniki[3]
+print(rozwiazWykladnicze(collection["1"][1],6.5))
 
-def rozwiazTrygonometryczne(funTryg, x, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY, wspolczynnikPrzyY):
-    if funTryg == "sin":
-        return wspolczynnikPrzyY  * math.sin(wspolczynnikPrzyX * x + wspolczynnikDoX) + wspolczynnikDoY
-    elif funTryg == "cos":
-        return wspolczynnikPrzyY * math.cos(wspolczynnikPrzyX * x + wspolczynnikDoX) + wspolczynnikDoY
-    elif funTryg == "tan":
-        return wspolczynnikPrzyY * math.tan(wspolczynnikPrzyX * x + wspolczynnikDoX) + wspolczynnikDoY
+def rozwiazTrygonometryczne(wspolczynniki, x ):
+    #wspolczynniki = [funTryg, wspolczynnikPrzyY, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY,]
+    if wspolczynniki[0] == "sin":
+        return wspolczynniki[1]  * math.sin(wspolczynniki[2] * x + wspolczynniki[3]) + wspolczynniki[4]
+    elif wspolczynniki[0] == "cos":
+        return wspolczynniki[1] * math.sin(wspolczynniki[2] * x + wspolczynniki[3]) + wspolczynniki[4]
+    elif wspolczynniki[0] == "tan":
+        return wspolczynniki[1] * math.sin(wspolczynniki[2] * x + wspolczynniki[3]) + wspolczynniki[4]
     else:
-        return 0
+        return 5
+print(rozwiazTrygonometryczne(collection["0"][1],2))
 
+def rozwiazRownanie(iloscZlozen, kolejnoscFunkcji):
+
+    return 0
 
 def metodaBisekcjiDokladnosc (wspolczynniki, a, b, dokladnosc):
 
@@ -63,7 +74,6 @@ def metodaBisekcjiIloscIteracji (wspolczynniki, a, b, iloscIteracji):
             a = srodek
         licznik += 1
 
-print(metodaBisekcjiDokladnosc([2,0,-1,-5],0, 100, 0.1))
-print(metodaBisekcjiIloscIteracji([2,0,-1,-5],0, 100, 100))
+
 
 
