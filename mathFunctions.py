b40aaec10066c2f9f4fe76dcffb7ed0b989935df
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-03-05 19:31:58 +0100

    kr00wa na 2 zlozenia zrobiona i dziala na zlozenie funkcji trygonometrycznych

--- mathFunctions.py
+++ mathFunctions.py
@@ -2,7 +2,8 @@ import math
 
 collection = {
     "0" : ["3sin(2x)", ["sin", 3, 2, 0,0]], #[funTryg, wspolczynnikPrzyY, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY,]
-    "1" : ["(2^3x-6)+11", [2,3,-6,11]], #podstawa, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY
+    "1" : ["(2^3x - 6) + 11", [2,3,-6,11]], #podstawa, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY
+    "2" : ["x^3 - 2x - 2", [1,0,-2,-2]], #wspolczynnikPrzyX^3, wspolczynnikPrzyX^2, wspolczynnikPrzyX^1, wyrazWolny
 
 }
 
@@ -12,18 +13,16 @@ def potegowanie (x,potega):
         wynik *=x
     return wynik
 def rozwiazWielomioan (wspolczynniki, x):
+    #wspolczynniki = [wspolczynnikPrzyX^3, wspolczynnikPrzyX^2, wspolczynnikPrzyX^1, wyrazWolny]
     wynik = 0
     for i in range(len(wspolczynniki)):
         wynik = wynik * x + wspolczynniki[i]
     return wynik
-
-
+print(rozwiazWielomioan(collection["2"][1],4.7))
 def rozwiazWykladnicze(wspolczynniki, x):
     #wspolczynniki = [podstawa, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY]
-    # return potegowanie(wspolczynniki[0],wspolczynniki[1] * x+ wspolczynniki[2]) + wspolczynniki[3]
     return pow(wspolczynniki[0],((wspolczynniki[1] * x +  wspolczynniki[2]) ))+ wspolczynniki[3]
 print(rozwiazWykladnicze(collection["1"][1],6.5))
-
 def rozwiazTrygonometryczne(wspolczynniki, x ):
     #wspolczynniki = [funTryg, wspolczynnikPrzyY, wspolczynnikPrzyX, wspolczynnikDoX, wspolczynnikDoY,]
     if wspolczynniki[0] == "sin":
@@ -35,11 +34,64 @@ def rozwiazTrygonometryczne(wspolczynniki, x ):
     else:
         return 5
 print(rozwiazTrygonometryczne(collection["0"][1],2))
+def rozwiazRownanie(iloscZlozen, kolejnoscFunkcji,x):
+    print(collection[kolejnoscFunkcji[0]][1])
+    print(kolejnoscFunkcji[0])
+    if iloscZlozen == 0:
+        match (kolejnoscFunkcji[0][0]):
+            case '0':
+                return rozwiazTrygonometryczne(collection[kolejnoscFunkcji[0]][1],x)
+            case '1':
+                return rozwiazWykladnicze(collection[kolejnoscFunkcji[0]][1],x)
+            case "2":
+                return rozwiazWielomioan(collection[kolejnoscFunkcji[0]][1],x)
+            case _:
+                return 9999
+    elif iloscZlozen == 1:
+        match (kolejnoscFunkcji[0][0]):
+            case '0':
+                match (kolejnoscFunkcji[1][0]):
+                    case '0':
+                        return rozwiazTrygonometryczne(collection[kolejnoscFunkcji[0]][1],rozwiazTrygonometryczne(collection[kolejnoscFunkcji[1]][1],x))
+                    case '1':
+                        return rozwiazTrygonometryczne(collection[kolejnoscFunkcji[0]][1],rozwiazWykladnicze(collection[kolejnoscFunkcji[1]][1], x))
+                    case '2':
+                        return rozwiazTrygonometryczne(collection[kolejnoscFunkcji[0]][1],rozwiazWielomioan(collection[kolejnoscFunkcji[1]][1], x))
+                    case _:
+                         return 9999
 
-def rozwiazRownanie(iloscZlozen, kolejnoscFunkcji):
+            case '1':
+                match (kolejnoscFunkcji[1][0]):
+                    case '0':
+                        return rozwiazWykladnicze(collection[kolejnoscFunkcji[0]][1],
+                                                       rozwiazTrygonometryczne(collection[kolejnoscFunkcji[0]][1], x))
+                    case '1':
+                        return rozwiazWykladnicze(collection[kolejnoscFunkcji[0]][1],
+                                                       rozwiazWykladnicze(collection[kolejnoscFunkcji[0]][1], x))
+                    case '2':
+                        return rozwiazWykladnicze(collection[kolejnoscFunkcji[0]][1],
+                                                       rozwiazWielomioan(collection[kolejnoscFunkcji[0]][1], x))
+                    case _:
+                        return 9999
+            case "2":
+                match (kolejnoscFunkcji[1][0]):
+                    case '0':
+                        return rozwiazWielomioan(collection[kolejnoscFunkcji[0]][1],
+                                                  rozwiazTrygonometryczne(collection[kolejnoscFunkcji[0]][1], x))
+                    case '1':
+                        return rozwiazWielomioan(collection[kolejnoscFunkcji[0]][1],
+                                                  rozwiazWykladnicze(collection[kolejnoscFunkcji[0]][1], x))
+                    case '2':
+                        return rozwiazWielomioan(collection[kolejnoscFunkcji[0]][1],
+                                                  rozwiazWielomioan(collection[kolejnoscFunkcji[0]][1], x))
+                    case _:
+                        return 9999
+            case _:
+                return 9999
 
     return 0
 
+print(rozwiazRownanie(1,["0","0"],4.7))
 def metodaBisekcjiDokladnosc (wspolczynniki, a, b, dokladnosc):
 
     while (abs(schematHornera(wspolczynniki, (a-b))) > dokladnosc):
