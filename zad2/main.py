af4345cf9ee34cf3fd7dfa1421b9f72794f5ec86
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-03-26 18:43:27 +0100

    dodano obsluge tych zabezpieczen

--- zad2/main.py
+++ zad2/main.py
@@ -3,7 +3,7 @@ import myFunctions as mf
 text0 = "\t\t***Witaj w programie do rozwiazywania rownan nieliniowych metoda Gaussa-Seidela***"
 text1 = "Wybierz w metode w jaki sposob chcesz wprowadzic dane:\n\t1 - z pliku\n\t2 - recznie wprowadze\n\t3 - zakoncz program\nTwoj wybor: "
 text2 = "Wybierz metode zakonczenia algorytmu:\n\t1 - iteracyjnie\n\t2 - dokladnosc\nTwoj wybor: "
-text3 = "Niestety, nie jest to macierz diagonalna, wpisz poprawne wartosci.\n\n"
+text3 = "Niestety, nie jest to macierz diagonalna, sprobuje ją przeksztalcic.\n\n"
 
 print(text0)
 while True:
@@ -21,9 +21,6 @@ while True:
     match wybor1:
         case "1":
             coefficients, constants = mf.readDataFromFile("data.txt")
-            if mf.ifCatercornered(coefficients) == False:
-                print(text3)
-                continue
         case "2":
             coefficientsCount = int(input("Podaj zadana ilosc niewiadomych: "))
             for i in range(coefficientsCount):
@@ -35,16 +32,24 @@ while True:
                 value2 = float(input("Podaj wspolczynnik przy x^{}: ".format(coefficientsCount-coefficientsCount)))
                 coefficients.append(temp)
                 constants.append(value2)
-            if mf.ifCatercornered(coefficients) == False:
-                print(text3)
-                continue
         case "3":
             break
         case _:
             raise Exception("Niepoprawny pierwszy wybor")
+
     equetionCounter = len(constants)
     x0 = [1] * equetionCounter
     gigaMatrix = mf.createMatrix(coefficients, constants)
+
+    if mf.ifCatercornered(coefficients) == False:
+        print(text3)
+        temp = mf.makeItCatercornered(gigaMatrix)
+        if temp != False:
+            print("Udalo sie przeksztalcic podana macierz do macierzy diagonalnie dominujaca!")
+            gigaMatrix = temp
+        else:
+            print("Nie udalo sie przeksztalcic podanej macierzy, ale i tak sprobujemy rozwiazac te rowania.")
+
     wybor2 = input(text2)
 
     match wybor2:
@@ -55,11 +60,15 @@ while True:
         case "2":
             precision = float(input("Podaj dokladnosc, po osiagnieciu ktorej algorytm ma sie zatrzymac: "))
             newX0, precisions, counter,flag = mf.precisionGaussSeidelMethod(gigaMatrix, x0, precision)
+            if flag == False:
+                print("Podany uklad rownan nie jest zbiezny, podany output jest ostatnia obliczona wartoscia.")
         case _:
             raise Exception("Niepoprawny drugi wybor")
 
     mowaKoncowa = "\n\n\t\t\tOUTPUT\n**Obliczone rozwiazania:\n\t{}\n**Dokladnosc dla kazdego rozwiazania:\n\t{}\n**Liczba iteracji:\n\t{}\n\n".format(newX0,precisions,counter)
     print(mowaKoncowa)
+    print("**Macierz:")
+    print(gigaMatrix)
 
 print("\nMilego dnia!")
 
